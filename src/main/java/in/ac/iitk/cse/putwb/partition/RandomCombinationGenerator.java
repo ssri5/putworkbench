2dcffcad736aba8bd9396bec8df2e927c0e7ee58
ssri <ssri@cse.iitk.ac.in>
2018-02-06 17:27:40 +0530

    PUTWorkbench 1.31
    Bug fix:
    ***************************
    Bug: The random attribute generation method may run into an infinite
    loop if the number of combinations to generate are more than possible,
    after considering privacy exceptions.
    Fix: Added a time-out mechanism to check such cases which checks
    periodically for progress in the generated pool, and stops the
    generation if it has been stuck for a considerable amount of time.
    ***************************

--- src/main/java/in/ac/iitk/cse/putwb/partition/RandomCombinationGenerator.java
+++ src/main/java/in/ac/iitk/cse/putwb/partition/RandomCombinationGenerator.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import java.util.TreeSet;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * A class to generate random combinations of a set of attributes.
@@ -18,25 +19,34 @@ import java.util.concurrent.ThreadLocalRandom;
 public class RandomCombinationGenerator implements Runnable { 
 	
 	/**
+	 * Signals any worker threads that they must stop attempting generation of a new combination
+	 */
+	private static AtomicBoolean stopGeneration;
+	
+	/**
 	 * A set of exceptions to honour
 	 */
 	private static Set<Set<Integer>> ignoreTogetherSet;
 	
 	/**
-	 * The set that contains the generated partitions at any point
+	 * If no new random combination could be generated in specified number of seconds, the generator gives up.
 	 */
-	private static Set<Set<Integer>> combinations;
+	private static final int TIMEOUT_IN_SECONDS = 60;
+	
+	/**
+	 * The thread-safe wrapper set that contains the generated partitions at any point
+	 */
+	private static Set<Set<Integer>> combinationsWrapper;
 	
 	/**
 	 * A list of numbers from 1 to n
 	 */
 	private static List<Integer> startingList;
 	
-	static {
-		ignoreTogetherSet = new HashSet<Set<Integer>>();
-		combinations = Collections.synchronizedSet(new HashSet<Set<Integer>>());
-		startingList = new ArrayList<Integer>();
-	}
+	/**
+	 * The last checked size of the combinations set, used to check progress
+	 */
+	private static int combinationsSize;
 	
 	/**
 	 * Generates a fixed number of random combinations for given values of n and k, honouring a given set of privacy exceptions.
@@ -48,38 +58,92 @@ public class RandomCombinationGenerator implements Runnable {
 	 * @throws Exception If something goes wrong while generating the combinations
 	 */
 	public static Set<Set<Integer>> generateRandomCombinations(int n, int k, int numberOfCombinationsToGenerate, Set<Set<Integer>> ignoreTogether) throws Exception {
-		ignoreTogetherSet.clear();
-		ignoreTogetherSet.addAll(ignoreTogether);
-		combinations.clear();
-		startingList.clear();
+		ignoreTogetherSet = new HashSet<Set<Integer>>(ignoreTogether);
+		Set<Set<Integer>> combinationsSet = new HashSet<Set<Integer>>();
+		combinationsWrapper = Collections.synchronizedSet(combinationsSet);
+		startingList = new ArrayList<Integer>();
+		stopGeneration = new AtomicBoolean(false);
+		combinationsSize = -1;
 		for(int i = 1; i <= n; i++)
 			startingList.add(i);
 		int noOfCores = Runtime.getRuntime().availableProcessors();
 		
 		if(noOfCores > 2) {
-			ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(noOfCores);
-			executor.setRemoveOnCancelPolicy(true);
-			while(combinations.size() < numberOfCombinationsToGenerate) {
-				executor.submit(new RandomCombinationGenerator(k));
+			while(combinationsWrapper.size() < numberOfCombinationsToGenerate && !stopGeneration.get()) {
+				ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(noOfCores);
+				executor.setRemoveOnCancelPolicy(true);
+				for(int i = 0; i < 100*noOfCores; i++)	//  Create workers in a batch of size (no. of cores X 100)
+					executor.submit(new RandomCombinationGenerator(k));
+				executor.shutdown();
+				while(executor.isTerminating()) {
+					if(!isProgressing()) {	// Problem !! no new combinations are getting generated, give up !!
+						executor.shutdownNow();
+						stopGeneration.set(true);
+						// If other threads need cleanup time, hint the scheduler to prioritize them
+						Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
+						Thread.yield();
+						// give some more time (5 seconds) for things to clear up !!
+						Thread.sleep(5000);
+						// check if everything is fine, if not, something is seriously wrong.. just exit !!
+						if(!executor.isTerminated())
+							System.exit(-1);
+						else
+							break;
+					}
+					if(combinationsWrapper.size() >= numberOfCombinationsToGenerate)
+						stopGeneration.set(true);
+					Thread.sleep(50);
+				}
 			}
-			executor.shutdownNow();
-			while(!executor.isTerminating())
-				Thread.sleep(500);
-			while(combinations.size() > numberOfCombinationsToGenerate) {
-				Iterator<Set<Integer>> it = combinations.iterator();
-				it.next();
-				it.remove();
+			synchronized (combinationsWrapper) {
+				Iterator<Set<Integer>> it = combinationsWrapper.iterator();
+				while(combinationsWrapper.size() > numberOfCombinationsToGenerate) {
+					it.next();
+					it.remove();
+				}
 			}
 		} else {
 			RandomCombinationGenerator generator = new RandomCombinationGenerator(k);
 			int temp = numberOfCombinationsToGenerate;
 			while(temp-- != 0) {
-				combinations.add(generator.generateRandomCombination());
+				Set<Integer> generatedCombination = generator.generateRandomCombination();
+				if(generatedCombination != null)
+					combinationsWrapper.add(generatedCombination);
 			}
+			stopGeneration.set(true);
 		}
-		return combinations;
+		return combinationsSet;
 	}
 	
+	/**
+	 * Checks if the generation process is <i>progressing</i> or not (which essentially means that 
+	 * the number of combinations generated so far is higher than that generated "some time ago").
+	 * @return <code>true</code> if the process is progressing, <code>false</code> otherwise
+	 */
+	private static boolean isProgressing() {
+		int currentSize = -1;
+		int timeToWait = TIMEOUT_IN_SECONDS;
+		synchronized (combinationsWrapper) {
+			currentSize = combinationsWrapper.size();
+		}
+		while(currentSize == combinationsSize) {
+			timeToWait--;
+			try {
+				Thread.sleep(1000);
+				synchronized (combinationsWrapper) {
+					currentSize = combinationsWrapper.size();
+				}
+			} catch (InterruptedException e) {
+				// Shouldn't really be here, but if we are, just say there has been no progress !!
+				return false;
+			}
+			if(timeToWait <= 0)
+				return false;
+		}
+		combinationsSize = currentSize;
+		return true;
+	}
+
 	/**
 	 * Contains a set of attributes while the generation is going on
 	 */
@@ -89,7 +153,7 @@ public class RandomCombinationGenerator implements Runnable {
 	 * The value of <i>k</i> in <b>(n choose k)</b>
 	 */
 	private int k;
-
+	
 	/**
 	 * Creates a new instance of a random combination generator, with the given value of <i>k</i>.
 	 * @param k The number of attributes in a particular combination
@@ -105,7 +169,7 @@ public class RandomCombinationGenerator implements Runnable {
 	 * @return <code>false</code> if the combination hasn't already been generated, <code>true</code> otherwise 
 	 */
 	private boolean alreadyGenerated() {
-		if(combinations.contains(combination))
+		if(combinationsWrapper.contains(combination))
 			return true;
 		return false;
 	}
@@ -124,10 +188,17 @@ public class RandomCombinationGenerator implements Runnable {
 				combination.add(integerList.remove(randomNum));
 				ctr--;
 			}
-		} while(!Thread.currentThread().isInterrupted() && (hasExceptions() || alreadyGenerated()));
-		return combination;
+			if(stopGeneration.get() || Thread.currentThread().isInterrupted())
+				return null;
+			else {
+				if(alreadyGenerated() || hasExceptions())
+					continue;
+				else
+					return combination;
+			}
+		} while(true);
 	}
-	
+
 	/**
 	 * Returns if the currently generated combination violates any privacy exceptions or not
 	 * @return <code>true</code> if the combination doesn't have any exceptions, <code>false</code> otherwise
@@ -139,13 +210,15 @@ public class RandomCombinationGenerator implements Runnable {
 		}
 		return false;
 	}
-
+	
 	/* (non-Javadoc)
 	 * @see java.lang.Runnable#run()
 	 */
 	@Override
 	public void run() {
-		combinations.add(generateRandomCombination());
+		Set<Integer> generatedCombination = generateRandomCombination();
+		if(generatedCombination != null)
+			combinationsWrapper.add(generatedCombination);
 	}
 	
 }
