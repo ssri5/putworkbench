82218f70f8935a5758061857a70f9632fc53c964
ssri <ssri@cse.iitk.ac.in>
2018-04-18 16:53:44 +0530

    Bug Fix:
    The install directory remained in the Base Directory even after the
    installation packages creation was over.

--- install/windows/install.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo This installation script attempts to add the PUTWorkbench directory to your user's Path variable
-pause
-for %%i in ("%~dp0") do SET "appdir=%%~fi"
-setx path "%path%;%appdir%"
-pause
--- install/windows/putwb-rec.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for %%i in ("%~dp0") do SET "appdir=%%~fi"
-SET "jarfile=putwb-1.51-complete.jar"
-SET "fullpath=%appdir%%jarfile%"
-java -cp "%fullpath%" in.ac.iitk.cse.putwb.experiment.RecoveryManager %*
